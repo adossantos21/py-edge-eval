62b2a0e06eacb77e3b2eb061d630e80b0f6ec230
CowbungaMk1 <ianpicklo@hotmail.com>
2025-09-11 21:25:22 -0500

    Miscellaneous file additions and edits

--- eval_alt.sh
+++ eval_alt.sh
@@ -1,15 +1,15 @@
 # On the following line, `cityscapes-raw-eval` was formerly `python scripts/cityscapes_raw.py`
 python pyEdgeEval/scripts/evaluate/cityscapes_raw.py \
 /home/robert.breslin/datasets/cityscapes \
-/home/robert.breslin/alessandro/paper_2/mmsegmentation/SBD/ablation07/preds/sbd_0 \
---output-path '/home/robert.breslin/alessandro/paper_2/mmsegmentation/SBD/ablation07/sbd_results' \
+/home/robert.breslin/alessandro/paper_2/mmsegmentation/SBD/ablation03/preds/edge4loss20/sbd_0 \
+--output-path '/home/robert.breslin/alessandro/paper_2/mmsegmentation/SBD/ablation03/sbd_results' \
+--categories '[1]' \
 --nonIS \
 --pre-seal \
 --remove-root \
 --max-dist 0.02 \
 --thresholds 99 \
---nproc 8 \
+--nproc 24 \
 --split 'val' \
 --pred-suffix '_SBD.png' \
---multi-label \
-# --categories '[1]' \ # Uncomment to evaluate on a specific category
+# --multi-label # Uncomment to evaluate SBD rather than Binary HED.
