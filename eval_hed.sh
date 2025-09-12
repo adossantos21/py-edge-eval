2743e9c8adfa984cd445b1d214667f532be3d86c
adossantos21 <alessandro.dossantos@ndus.edu>
2025-09-12 12:21:53 -0500

    Cleaned up eval_hed.sh and eval_sbd.sh

--- eval_hed.sh
+++ eval_hed.sh
@@ -1,8 +1,7 @@
-# On the following line, `cityscapes-raw-eval` was formerly `python scripts/cityscapes_raw.py`
 python pyEdgeEval/scripts/evaluate/cityscapes_raw.py \
-/home/robert.breslin/datasets/cityscapes \
-/home/robert.breslin/alessandro/paper_2/mmsegmentation/SBD/ablation03/preds/edge4loss20/sbd_0 \
---output-path '/home/robert.breslin/alessandro/paper_2/mmsegmentation/SBD/ablation03/sbd_results' \
+<path_to_cityscapes_root> \
+<path_to_sbd_predictions> \
+--output-path <output_path> \
 --categories '[1]' \
 --nonIS \
 --pre-seal \
@@ -12,4 +11,3 @@ python pyEdgeEval/scripts/evaluate/cityscapes_raw.py \
 --nproc 24 \
 --split 'val' \
 --pred-suffix '_SBD.png' \
-# --multi-label # Uncomment to evaluate SBD rather than Binary HED.
