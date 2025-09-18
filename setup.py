3775ac02746f50af1c63b28d2d2fd932b62947c6
CowbungaMk1 <ianpicklo@hotmail.com>
2025-09-18 10:17:41 -0500

    Added generate-cityscapes-sbd-gt and generate-cityscapes-hed-gt executables

--- setup.py
+++ setup.py
@@ -171,7 +171,8 @@ setup(
             'cityscapes-raw-eval = pyEdgeEval.scripts.evaluate.cityscapes_raw:main',
             'cityscapes-thin-eval = pyEdgeEval.scripts.evaluate.cityscapes_thin:main',
             'bsds500 = pyEdgeEval.scripts.evaluate.bsds500:main',
-            'generate-cityscapes-gt = pyEdgeEval.scripts.convert_datasets.cityscapes:main',
+            'generate-cityscapes-sbd-gt = pyEdgeEval.scripts.convert_datasets.cityscapes:main',
+            'generate-cityscapes-hed-gt = pyEdgeEval.scripts.convert_datasets.cityscapes_hed:main',
         ],
     },
 )
